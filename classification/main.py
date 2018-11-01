0117b1c5f1c5dbc27973d7bcb1d69c371be7bdcf
master8 <master8.michael@gmail.com>
2018-11-01 23:25:01 +0500

    эксперимент

--- classification/main.py
+++ classification/main.py
@@ -73,10 +73,10 @@ def run_experiments(corpus_name, x_column_name, y_column_name):
 
 # sz - count vacancies per mark
 # m - count marks
-# CURRENT_CORPUS_NAME = 'hh_corpus_sz245_m20_all'
-#
-# CURRENT_X_COLUMN_NAME = 'all_description'
-# CURRENT_Y_COLUMN_NAME = 'standard_mark'
+CURRENT_CORPUS_NAME = 'hh_corpus_sz245_m20_all_v2'
+
+CURRENT_X_COLUMN_NAME = 'all_description'
+CURRENT_Y_COLUMN_NAME = 'standard_mark'
 #
 # run_experiments(corpus_name='hh_sz100_m20_nrd',
 #                 x_column_name='name_requirements_duties',
@@ -107,28 +107,28 @@ def run_experiments(corpus_name, x_column_name, y_column_name):
 #                 x_column_name='name_requirements_duties',
 #                 y_column_name='standard_mark')
 
-# data_source = DataSource(CURRENT_CORPUS_NAME,
-#                          CURRENT_X_COLUMN_NAME,
-#                          CURRENT_Y_COLUMN_NAME)
+data_source = DataSource(CURRENT_CORPUS_NAME,
+                         CURRENT_X_COLUMN_NAME,
+                         CURRENT_Y_COLUMN_NAME)
 
-# tokenizer = Tokenizer(data_source=data_source,
-#                       corpus_name=CURRENT_CORPUS_NAME)
-# tokenizer.tokenize()
+tokenizer = Tokenizer(data_source=data_source,
+                      corpus_name=CURRENT_CORPUS_NAME)
+tokenizer.tokenize()
 
-# tokens_provider = TokensProvider(corpus_name=CURRENT_CORPUS_NAME)
+tokens_provider = TokensProvider(corpus_name=CURRENT_CORPUS_NAME)
 
-# vectorizer = Vectorizer(tokens_provider=tokens_provider,
-#                         corpus_name=CURRENT_CORPUS_NAME)
-# vectorizer.vectorize_with_tfidf()
-# vectorizer.vectorize_with_w2v()
+vectorizer = Vectorizer(tokens_provider=tokens_provider,
+                        corpus_name=CURRENT_CORPUS_NAME)
+vectorizer.vectorize_with_tfidf()
+vectorizer.vectorize_with_w2v()
 # vectorizer.vectorize_with_w2v_tfidf()
 # vectorizer.vectorize_with_w2v_big()
 # vectorizer.vectorize_with_tfidf_wshingles()
 # vectorizer.vectorize_with_tfidf_ngrams()
 # vectorizer.vectorize_with_w2v_old()
 
-# vectors_provider = VectorsProvider(corpus_name=CURRENT_CORPUS_NAME)
-# visualizer = Visualizer(corpus_name=CURRENT_CORPUS_NAME)
+vectors_provider = VectorsProvider(corpus_name=CURRENT_CORPUS_NAME)
+visualizer = Visualizer(corpus_name=CURRENT_CORPUS_NAME)
 
 # x_all = vectors_provider.get_w2v_vectors()
 # y_all = data_source.get_y()
@@ -167,11 +167,11 @@ def run_experiments(corpus_name, x_column_name, y_column_name):
 # co[proba_pred_column] = temp[proba_pred_column]
 # data_source.save_corpus(co)
 
-# logreg = LogisticRegressionExperiments(data_source=data_source,
-#                                        vectors_provider=vectors_provider,
-#                                        visualizer=visualizer)
-# logreg.make_use_w2v()
-# logreg.make_use_tfidf()
+logreg = LogisticRegressionExperiments(data_source=data_source,
+                                       vectors_provider=vectors_provider,
+                                       visualizer=visualizer)
+logreg.make_use_w2v()
+logreg.make_use_tfidf()
 # logreg.make_use_w2v_with_tfidf(
 # logreg.make_use_w2v_big())
 # logreg.make_use_tfidf_wshingles()
