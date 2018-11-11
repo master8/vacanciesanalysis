665c280ac6bc95aac857a2827d85b76da39240cb
master8 <master8.michael@gmail.com>
2018-11-11 23:54:33 +0500

    поправил мёрдж

--- classification/main.py
+++ classification/main.py
@@ -79,7 +79,7 @@ def run_experiments(corpus_name, x_column_name, y_column_name):
 
 # sz - count vacancies per mark
 # m - count marks
-CURRENT_CORPUS_NAME = 'hh_corpus_sz245_m20_all_v4'
+CURRENT_CORPUS_NAME = 'hh_corpus_sz245_m20_all_v5'
 
 CURRENT_X_COLUMN_NAME = 'all_description'
 CURRENT_Y_COLUMN_NAME = 'standard_mark'
@@ -116,14 +116,14 @@ data_source = DataSource(CURRENT_CORPUS_NAME,
 # vectorizer.vectorize_with_tfidf_ngrams()
 # vectorizer.vectorize_with_w2v_old()
 
-vectors_provider = VectorsProvider(corpus_name=CURRENT_CORPUS_NAME)
-visualizer = Visualizer(corpus_name=CURRENT_CORPUS_NAME)
-
-onevsrest = OneVsRestExperiments(data_source=data_source,
-                                 vectors_provider=vectors_provider,
-                                 visualizer=visualizer)
+# vectors_provider = VectorsProvider(corpus_name=CURRENT_CORPUS_NAME)
+# visualizer = Visualizer(corpus_name=CURRENT_CORPUS_NAME)
+#
+# onevsrest = OneVsRestExperiments(data_source=data_source,
+#                                  vectors_provider=vectors_provider,
+#                                  visualizer=visualizer)
 # onevsrest.make_use_tfidf()
-onevsrest.make_use_w2v()
+# onevsrest.make_use_w2v()
 
 # x_all = vectors_provider.get_w2v_vectors()
 # y_all = data_source.get_y_multi_label()
@@ -247,9 +247,9 @@ onevsrest.make_use_w2v()
 
 
 # merge_marking(
-#     corpus_original_name='hh_corpus_sz245_m20_all_v3.csv',
-#     corpus_edited_name='hh_corpus_sz245_m20_all_v3_edit.csv',
-#     corpus_result_name='hh_corpus_sz245_m20_all_v4.csv'
+#     corpus_original_name='hh_corpus_sz245_m20_all_v4.csv',
+#     corpus_edited_name='hh_corpus_sz245_m20_all_v4_edit.csv',
+#     corpus_result_name='hh_corpus_sz245_m20_all_v5.csv'
 # )
 
 
