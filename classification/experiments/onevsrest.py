60d2ffaf586d45b95280cc2bf92d50226eb54de4
master8 <master8.michael@gmail.com>
2018-11-15 22:17:50 +0500

    остатки w2v и tfidf

--- classification/experiments/onevsrest.py
+++ classification/experiments/onevsrest.py
@@ -46,13 +46,13 @@ class OneVsRestExperiments:
         # TODO here grid search
 
         base_estimators = [
-            LogisticRegression(n_jobs=-1),
+            # LogisticRegression(n_jobs=-1),
             # SVC(),
             # KNeighborsClassifier(),
             # GaussianProcessClassifier(),
             # DecisionTreeClassifier(),
             # RandomForestClassifier(),
-            MLPClassifier(),
+            # MLPClassifier(),
             # AdaBoostClassifier(),
             # GaussianNB(),
             # QuadraticDiscriminantAnalysis()
@@ -61,17 +61,30 @@ class OneVsRestExperiments:
             # Perceptron(n_jobs=-1),
             # PassiveAggressiveClassifier(n_jobs=-1),
             # BernoulliNB(),
-            LinearSVC()
+            # LinearSVC(),
+            BRkNNaClassifier(),
+            BRkNNbClassifier(),
+            MLkNN(),
+            MLARAM(),
+            BinaryRelevance(LogisticRegression(n_jobs=-1)),
+            BinaryRelevance(LinearSVC()),
+            BinaryRelevance(MLPClassifier()),
+            ClassifierChain(LogisticRegression(n_jobs=-1)),
+            ClassifierChain(LinearSVC()),
+            ClassifierChain(MLPClassifier()),
+            LabelPowerset(LogisticRegression(n_jobs=-1)),
+            LabelPowerset(LinearSVC()),
+            LabelPowerset(MLPClassifier())
         ]
 
         model_params = [
-            'LogisticRegression()',
+            # 'LogisticRegression()',
             # 'SVC()',
             # 'KNeighborsClassifier()',
             # 'GaussianProcessClassifier()',
             # 'DecisionTreeClassifier()',
             # 'RandomForestClassifier()',
-            'MLPClassifier()',
+            # 'MLPClassifier()',
             # 'AdaBoostClassifier()',
             # 'GaussianNB()',
             # 'QuadraticDiscriminantAnalysis()'
@@ -80,16 +93,35 @@ class OneVsRestExperiments:
             # 'Perceptron()',
             # 'PassiveAggressiveClassifier()',
             # 'BernoulliNB()',
-            'LinearSVC()'
+            # 'LinearSVC()',
+            'BRkNNaClassifier()',
+            'BRkNNbClassifier()',
+            'MLkNN()',
+            'MLARAM()',
+            'BinaryRelevance(LogisticRegression())',
+            'BinaryRelevance(LinearSVC())',
+            'BinaryRelevance(MLPClassifier())',
+            'ClassifierChain(LogisticRegression())',
+            'ClassifierChain(LinearSVC())',
+            'ClassifierChain(MLPClassifier())',
+            'LabelPowerset(LogisticRegression())',
+            'LabelPowerset(LinearSVC())',
+            'LabelPowerset(MLPClassifier())'
         ]
 
         i = 0
         for base_estimator in base_estimators:
-            model = OneVsRestClassifier(base_estimator, n_jobs=-1)
-            cross_val_f1 = Evaluator.evaluate_only_cross_val(model, x_all, y_all)
-            self.__visualizer.show_results_briefly(self.__CLASSIFIER_NAME, model_params[i],
-                                                   "tfidf", cross_val_f1)
+            logging.warning('Start ' + model_params[i])
+            try:
+                # model = OneVsRestClassifier(base_estimator, n_jobs=-1)
+                model = base_estimator
+                cross_val_f1 = Evaluator.evaluate_only_cross_val(model, x_all, y_all)
+                self.__visualizer.show_results_briefly(self.__CLASSIFIER_NAME, model_params[i],
+                                                       "tfidf", cross_val_f1)
+            except:
+                logging.warning('Error on ' + model_params[i])
             i += 1
+            logging.warning('End ' + model_params[i])
 
         # model1 = OneVsRestClassifier(LogisticRegression(C=1.0, solver='sag', n_jobs=-1), n_jobs=-1)
         # Evaluator.multi_label_predict_proba_tfidf(model1, x_all, y_all, data_source=self.__data_source)
@@ -105,120 +137,110 @@ class OneVsRestExperiments:
 
         # TODO here grid search
 
-        # base_estimators = [
-        #     # LogisticRegression(n_jobs=-1),
-        #     # SVC(),
-        #     # KNeighborsClassifier(),
-        #     # GaussianProcessClassifier(),
-        #     # DecisionTreeClassifier(),
-        #     # RandomForestClassifier(),
-        #     # MLPClassifier(),
-        #     # AdaBoostClassifier(),
-        #     # GaussianNB(),
-        #     # QuadraticDiscriminantAnalysis()
-        #     # RidgeClassifier(),
-        #     # SGDClassifier(n_jobs=-1),
-        #     # Perceptron(n_jobs=-1),
-        #     # PassiveAggressiveClassifier(n_jobs=-1),
-        #     # BernoulliNB(),
-        #     # LinearSVC(),
-        #     # LinearDiscriminantAnalysis(),
-        #     # BaggingClassifier(LogisticRegression(n_jobs=-1), n_jobs=-1),
-        #     # BaggingClassifier(LinearSVC(), n_jobs=-1),
-        #     # BaggingClassifier(MLPClassifier(), n_jobs=-1),
-        #     # ExtraTreesClassifier(n_jobs=-1),
-        #     # GradientBoostingClassifier(),
-        #     #? VotingClassifier([LogisticRegression(n_jobs=-1), LinearSVC(), MLPClassifier()], n_jobs=-1),
-        #     #? LogisticRegressionCV(n_jobs=-1),
-        #     #? RidgeClassifierCV(),
-        #     #? MultinomialNB(),
-        #     BRkNNaClassifier(),
-        #     BRkNNbClassifier(),
-        #     MLkNN(),
-        #     MLARAM(),
-        #     BinaryRelevance(LogisticRegression(n_jobs=-1)),
-        #     BinaryRelevance(LinearSVC()),
-        #     BinaryRelevance(MLPClassifier()),
-        #     ClassifierChain(LogisticRegression(n_jobs=-1)),
-        #     ClassifierChain(LinearSVC()),
-        #     ClassifierChain(MLPClassifier()),
-        #     LabelPowerset(LogisticRegression(n_jobs=-1)),
-        #     LabelPowerset(LinearSVC()),
-        #     LabelPowerset(MLPClassifier()),
-        #     RakelD(LogisticRegression(n_jobs=-1)),
-        #     RakelD(LinearSVC()),
-        #     RakelD(MLPClassifier()),
-        #     RakelO(LogisticRegression(n_jobs=-1)),
-        #     RakelO(LinearSVC()),
-        #     RakelO(MLPClassifier())
-        # ]
+        base_estimators = [
+            # LogisticRegression(n_jobs=-1),
+            # SVC(),
+            # KNeighborsClassifier(),
+            # GaussianProcessClassifier(),
+            # DecisionTreeClassifier(),
+            # RandomForestClassifier(),
+            # MLPClassifier(),
+            # AdaBoostClassifier(),
+            # GaussianNB(),
+            # QuadraticDiscriminantAnalysis()
+            # RidgeClassifier(),
+            # SGDClassifier(n_jobs=-1),
+            # Perceptron(n_jobs=-1),
+            # PassiveAggressiveClassifier(n_jobs=-1),
+            # BernoulliNB(),
+            # LinearSVC(),
+            # LinearDiscriminantAnalysis(),
+            # BaggingClassifier(LogisticRegression(n_jobs=-1), n_jobs=-1),
+            # BaggingClassifier(LinearSVC(), n_jobs=-1),
+            # BaggingClassifier(MLPClassifier(), n_jobs=-1),
+            # ExtraTreesClassifier(n_jobs=-1),
+            # GradientBoostingClassifier(),
+            #? VotingClassifier([LogisticRegression(n_jobs=-1), LinearSVC(), MLPClassifier()], n_jobs=-1),
+            LogisticRegressionCV(n_jobs=-1),
+            RidgeClassifierCV(),
+            MultinomialNB(),
+            # BRkNNaClassifier(),
+            # BRkNNbClassifier(),
+            # MLkNN(),
+            # MLARAM(),
+            # BinaryRelevance(LogisticRegression(n_jobs=-1)),
+            # BinaryRelevance(LinearSVC()),
+            # BinaryRelevance(MLPClassifier()),
+            # ClassifierChain(LogisticRegression(n_jobs=-1)),
+            # ClassifierChain(LinearSVC()),
+            # ClassifierChain(MLPClassifier()),
+            # LabelPowerset(LogisticRegression(n_jobs=-1)),
+            # LabelPowerset(LinearSVC()),
+            # LabelPowerset(MLPClassifier())
+        ]
+
+        model_params = [
+            # 'LogisticRegression()',
+            # 'SVC()',
+            # 'KNeighborsClassifier()',
+            # 'GaussianProcessClassifier()',
+            # 'DecisionTreeClassifier()',
+            # 'RandomForestClassifier()',
+            # 'MLPClassifier()',
+            # 'AdaBoostClassifier()',
+            # 'GaussianNB()',
+            # 'QuadraticDiscriminantAnalysis()'
+            # 'RidgeClassifier()',
+            # 'SGDClassifier()',
+            # 'Perceptron()',
+            # 'PassiveAggressiveClassifier()',
+            # 'BernoulliNB()',
+            # 'LinearSVC()',
+            # 'LinearDiscriminantAnalysis()',
+            # 'BaggingClassifier(LogisticRegression())',
+            # 'BaggingClassifier(LinearSVC())',
+            # 'BaggingClassifier(MLPClassifier())',
+            # 'ExtraTreesClassifier()',
+            # 'GradientBoostingClassifier()',
+            # 'VotingClassifier([LogisticRegression(), LinearSVC(), MLPClassifier()])',
+            'LogisticRegressionCV()',
+            'RidgeClassifierCV()',
+            'MultinomialNB()',
+            # 'BRkNNaClassifier()',
+            # 'BRkNNbClassifier()',
+            # 'MLkNN()',
+            # 'MLARAM()',
+            # 'BinaryRelevance(LogisticRegression())',
+            # 'BinaryRelevance(LinearSVC())',
+            # 'BinaryRelevance(MLPClassifier())',
+            # 'ClassifierChain(LogisticRegression())',
+            # 'ClassifierChain(LinearSVC())',
+            # 'ClassifierChain(MLPClassifier())',
+            # 'LabelPowerset(LogisticRegression())',
+            # 'LabelPowerset(LinearSVC())',
+            # 'LabelPowerset(MLPClassifier())'
+        ]
+
+        i = 0
+        for base_estimator in base_estimators:
+            logging.warning('Start ' + model_params[i])
+            try:
+                # model = OneVsRestClassifier(base_estimator, n_jobs=-1)
+                model = base_estimator
+                cross_val_f1 = Evaluator.evaluate_only_cross_val(model, x_all, y_all)
+                self.__visualizer.show_results_briefly(self.__CLASSIFIER_NAME, model_params[i],
+                                               "Word2Vec", cross_val_f1)
+            except:
+                logging.warning('Error on ' + model_params[i])
+            i += 1
+            logging.warning('End ' + model_params[i])
+
+
+
+        # model1 = OneVsRestClassifier(LogisticRegression(C=1.0, solver='sag', n_jobs=-1), n_jobs=-1)
+        # Evaluator.multi_label_predict_proba_w2v(model1, x_all, y_all, data_source=self.__data_source)
         #
-        # model_params = [
-        #     # 'LogisticRegression()',
-        #     # 'SVC()',
-        #     # 'KNeighborsClassifier()',
-        #     # 'GaussianProcessClassifier()',
-        #     # 'DecisionTreeClassifier()',
-        #     # 'RandomForestClassifier()',
-        #     # 'MLPClassifier()',
-        #     # 'AdaBoostClassifier()',
-        #     # 'GaussianNB()',
-        #     # 'QuadraticDiscriminantAnalysis()'
-        #     # 'RidgeClassifier()',
-        #     # 'SGDClassifier()',
-        #     # 'Perceptron()',
-        #     # 'PassiveAggressiveClassifier()',
-        #     # 'BernoulliNB()',
-        #     # 'LinearSVC()',
-        #     # 'LinearDiscriminantAnalysis()',
-        #     # 'BaggingClassifier(LogisticRegression())',
-        #     # 'BaggingClassifier(LinearSVC())',
-        #     # 'BaggingClassifier(MLPClassifier())',
-        #     # 'ExtraTreesClassifier()',
-        #     # 'GradientBoostingClassifier()',
-        #     # 'VotingClassifier([LogisticRegression(), LinearSVC(), MLPClassifier()])',
-        #     # 'LogisticRegressionCV()',
-        #     # 'RidgeClassifierCV()',
-        #     # 'MultinomialNB()',
-        #     'BRkNNaClassifier()',
-        #     'BRkNNbClassifier()',
-        #     'MLkNN()',
-        #     'MLARAM()',
-        #     'BinaryRelevance(LogisticRegression())',
-        #     'BinaryRelevance(LinearSVC())',
-        #     'BinaryRelevance(MLPClassifier())',
-        #     'ClassifierChain(LogisticRegression())',
-        #     'ClassifierChain(LinearSVC())',
-        #     'ClassifierChain(MLPClassifier())',
-        #     'LabelPowerset(LogisticRegression())',
-        #     'LabelPowerset(LinearSVC())',
-        #     'LabelPowerset(MLPClassifier())',
-        #     'RakelD(LogisticRegression())',
-        #     'RakelD(LinearSVC())',
-        #     'RakelD(MLPClassifier())',
-        #     'RakelO(LogisticRegression())',
-        #     'RakelO(LinearSVC())',
-        #     'RakelO(MLPClassifier())'
-        # ]
+        # cross_val_f1 = Evaluator.evaluate_only_cross_val(model1, x_all, y_all)
         #
-        # i = 0
-        # for base_estimator in base_estimators:
-        #     logging.warning('Start ' + model_params[i])
-        #     try:
-        #         # model = OneVsRestClassifier(base_estimator, n_jobs=-1)
-        #         model = base_estimator
-        #         cross_val_f1 = Evaluator.evaluate_only_cross_val(model, x_all, y_all)
-        #         self.__visualizer.show_results_briefly(self.__CLASSIFIER_NAME, model_params[i],
-        #                                        "Word2Vec", cross_val_f1)
-        #     except:
-        #         logging.warning('Error on ' + model_params[i])
-        #     i += 1
-        #     logging.warning('End ' + model_params[i])
-
-        model1 = OneVsRestClassifier(LogisticRegression(C=1.0, solver='sag', n_jobs=-1), n_jobs=-1)
-        Evaluator.multi_label_predict_proba_w2v(model1, x_all, y_all, data_source=self.__data_source)
-
-        cross_val_f1 = Evaluator.evaluate_only_cross_val(model1, x_all, y_all)
-
-        self.__visualizer.show_results_briefly(self.__CLASSIFIER_NAME, "LogisticRegression(C=1.0, solver='sag')",
-                                       "Word2Vec", cross_val_f1)
+        # self.__visualizer.show_results_briefly(self.__CLASSIFIER_NAME, "LogisticRegression(C=1.0, solver='sag')",
+        #                                "Word2Vec", cross_val_f1)
--- classification/main.py
+++ classification/main.py
@@ -79,7 +79,7 @@ def run_experiments(corpus_name, x_column_name, y_column_name):
 
 # sz - count vacancies per mark
 # m - count marks
-CURRENT_CORPUS_NAME = 'hh_corpus_sz245_m20_all_v5'
+CURRENT_CORPUS_NAME = 'hh_corpus_sz245_m20_all_v4'
 
 CURRENT_X_COLUMN_NAME = 'all_description'
 CURRENT_Y_COLUMN_NAME = 'standard_mark'
@@ -104,26 +104,26 @@ data_source = DataSource(CURRENT_CORPUS_NAME,
 #                       corpus_name=CURRENT_CORPUS_NAME)
 # tokenizer.tokenize()
 #
-# tokens_provider = TokensProvider(corpus_name=CURRENT_CORPUS_NAME)
+tokens_provider = TokensProvider(corpus_name=CURRENT_CORPUS_NAME)
 #
-# vectorizer = Vectorizer(tokens_provider=tokens_provider,
-#                         corpus_name=CURRENT_CORPUS_NAME)
-# vectorizer.vectorize_with_tfidf()
-# vectorizer.vectorize_with_w2v()
+vectorizer = Vectorizer(tokens_provider=tokens_provider,
+                        corpus_name=CURRENT_CORPUS_NAME)
+vectorizer.vectorize_with_tfidf()
+vectorizer.vectorize_with_w2v()
 # vectorizer.vectorize_with_w2v_tfidf()
 # vectorizer.vectorize_with_w2v_big()
 # vectorizer.vectorize_with_tfidf_wshingles()
 # vectorizer.vectorize_with_tfidf_ngrams()
 # vectorizer.vectorize_with_w2v_old()
 
-# vectors_provider = VectorsProvider(corpus_name=CURRENT_CORPUS_NAME)
-# visualizer = Visualizer(corpus_name=CURRENT_CORPUS_NAME)
+vectors_provider = VectorsProvider(corpus_name=CURRENT_CORPUS_NAME)
+visualizer = Visualizer(corpus_name=CURRENT_CORPUS_NAME)
 #
-# onevsrest = OneVsRestExperiments(data_source=data_source,
-#                                  vectors_provider=vectors_provider,
-#                                  visualizer=visualizer)
-# onevsrest.make_use_tfidf()
-# onevsrest.make_use_w2v()
+onevsrest = OneVsRestExperiments(data_source=data_source,
+                                 vectors_provider=vectors_provider,
+                                 visualizer=visualizer)
+onevsrest.make_use_w2v()
+onevsrest.make_use_tfidf()
 
 # x_all = vectors_provider.get_w2v_vectors()
 # y_all = data_source.get_y_multi_label()
