bf3f93fa940459a8063c37a7879d6724db620563
master8 <master8.michael@gmail.com>
2018-11-19 23:20:53 +0500

    эксперименты

--- classification/experiments/labelpowerset.py
+++ classification/experiments/labelpowerset.py
@@ -45,78 +45,92 @@ class LabelPowersetExperiments:
         x_all = self.__vectors_provider.get_tfidf_vectors()
         y_all = self.__data_source.get_y_multi_label()
 
-        binarizer = MultiLabelBinarizer()
-        y_all = binarizer.fit_transform(y_all)
+        # binarizer = MultiLabelBinarizer()
+        # y_all = binarizer.fit_transform(y_all)
+        #
+        # default = LabelPowerset()
+        # parameters = [
+        #     {
+        #         'classifier': [SVC()],
+        #         'classifier__C': np.logspace(-1, 5, 7),
+        #         'classifier__kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+        #         'classifier__degree': range(0, 5, 1),
+        #         'classifier__probability': [True, False],
+        #         'classifier__shrinking': [True, False],
+        #
+        #     }
+        # ]
+        #
+        # grid = GridSearchCV(default, parameters, scoring='f1_weighted', cv=5, n_jobs=-1)
+        # grid.fit(x_all, y_all)
+        # best_param = grid.best_params_
+        # self.__visualizer.save_best_params(self.__CLASSIFIER_NAME, 'SVC()', "tfidf", best_param)
+        #
+        # logging.warning(str(datetime.now()) + 'End SVC')
+        #
+        # default = LabelPowerset()
+        #
+        # parameters = [
+        #     {
+        #         'classifier': [LogisticRegression()],
+        #         'classifier__fit_intercept': (True, False),
+        #         'classifier__C': np.logspace(-1, 5, 7),
+        #         'classifier__intercept_scaling': (True, False),
+        #         'classifier__class_weight': (None, 'balanced'),
+        #         'classifier__solver': ('newton-cg', 'lbfgs', 'sag', 'saga'),
+        #         'classifier__multi_class': ('ovr', 'multinomial')
+        #     }]
+        #
+        # grid = GridSearchCV(default, parameters, scoring='f1_weighted', cv=5, n_jobs=-1)
+        # grid.fit(x_all, y_all)
+        # best_param = grid.best_params_
+        # self.__visualizer.save_best_params(self.__CLASSIFIER_NAME, 'LogisticRegression()', "tfidf", best_param)
+        #
+        # logging.warning(str(datetime.now()) + 'End LogisticRegression')
 
-        default = LabelPowerset()
-        parameters = [
-            {
-                'classifier': [SVC()],
-                'classifier__C': np.logspace(-1, 5, 7),
-                'classifier__kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
-                'classifier__degree': range(0, 5, 1),
-                'classifier__probability': [True, False],
-                'classifier__shrinking': [True, False],
-
-            }
+        base_estimators = [
+            LogisticRegression(C=1.0, solver='sag', n_jobs=-1),
+            LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+                               intercept_scaling=True, max_iter=100, multi_class='ovr', penalty='l2', random_state=None,
+                               solver='newton-cg',
+                               tol=0.0001, warm_start=False, n_jobs=-1),
+            # LinearSVC(),
+            MLPClassifier(),
+            SVC(C=0.1, cache_size=200, class_weight=None, coef0=0.0,
+                decision_function_shape='ovr', degree=0, gamma='auto', kernel='linear',
+                max_iter=-1, probability=True, random_state=None, shrinking=True,
+                tol=0.001, verbose=False)
         ]
 
-        grid = GridSearchCV(default, parameters, scoring='f1_weighted', cv=5, n_jobs=-1)
-        grid.fit(x_all, y_all)
-        best_param = grid.best_params_
-        self.__visualizer.save_best_params(self.__CLASSIFIER_NAME, 'SVC()', "tfidf", best_param)
+        model_params = [
+            "LogisticRegression(C=1.0, solver='sag')",
+            "LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,\
+                               intercept_scaling=True, max_iter=100, multi_class='ovr', penalty='l2', random_state=None,\
+                               solver='newton-cg',\
+                               tol=0.0001, warm_start=False, n_jobs=-1)",
+            # "LinearSVC()",
+            "MLPClassifier()",
+            "SVC(C=0.1, cache_size=200, class_weight=None, coef0=0.0,\
+                decision_function_shape='ovr', degree=0, gamma='auto', kernel='linear',\
+                max_iter=-1, probability=True, random_state=None, shrinking=True,\
+                tol=0.001, verbose=False)"
+        ]
 
-        logging.warning(str(datetime.now()) + 'End SVC')
-
-        default = LabelPowerset()
-
-        parameters = [
-            {
-                'classifier': [LogisticRegression()],
-                'classifier__fit_intercept': (True, False),
-                'classifier__C': np.logspace(-1, 5, 7),
-                'classifier__intercept_scaling': (True, False),
-                'classifier__class_weight': (None, 'balanced'),
-                'classifier__solver': ('newton-cg', 'lbfgs', 'sag', 'saga'),
-                'classifier__multi_class': ('ovr', 'multinomial')
-            }]
-
-        grid = GridSearchCV(default, parameters, scoring='f1_weighted', cv=5, n_jobs=-1)
-        grid.fit(x_all, y_all)
-        best_param = grid.best_params_
-        self.__visualizer.save_best_params(self.__CLASSIFIER_NAME, 'LogisticRegression()', "tfidf", best_param)
-
-        logging.warning(str(datetime.now()) + 'End LogisticRegression')
-
-        # base_estimators = [
-        #     # LogisticRegression(C=1.0, solver='sag', n_jobs=-1),
-        #     LogisticRegression(n_jobs=-1),
-        #     LinearSVC(),
-        #     # MLPClassifier()
-        # ]
-        #
-        # model_params = [
-        #     # "LogisticRegression(C=1.0, solver='sag')",
-        #     "LogisticRegression()",
-        #     "LinearSVC()",
-        #     # "MLPClassifier()"
-        # ]
-        #
-        # i = 0
-        # for base_estimator in base_estimators:
-        #     logging.warning(str(datetime.now()) + 'Start ' + model_params[i])
-        #     try:
-        #         model = LabelPowerset(base_estimator)
-        #         # cross_val_f1 = Evaluator.evaluate_only_cross_val(model, x_all, y_all)
-        #         # self.__visualizer.show_results_briefly(self.__CLASSIFIER_NAME, model_params[i],
-        #         #                                        "Word2Vec_CBOW", cross_val_f1)
-        #         report, micro, macro, weighted = Evaluator.multi_label_report(model, x_all, y_all, True)
-        #         self.__visualizer.save_metrics(self.__CLASSIFIER_NAME, model_params[i], "tfidf",
-        #                                        report, micro, macro, weighted)
-        #     except:
-        #         logging.warning('Error on ' + model_params[i])
-        #     logging.warning(str(datetime.now()) + 'End ' + model_params[i])
-        #     i += 1
+        i = 0
+        for base_estimator in base_estimators:
+            logging.warning(str(datetime.now()) + 'Start ' + model_params[i])
+            try:
+                model = LabelPowerset(base_estimator)
+                # cross_val_f1 = Evaluator.evaluate_only_cross_val(model, x_all, y_all)
+                # self.__visualizer.show_results_briefly(self.__CLASSIFIER_NAME, model_params[i],
+                #                                        "Word2Vec_CBOW", cross_val_f1)
+                report, micro, macro, weighted = Evaluator.multi_label_report(model, x_all, y_all, True)
+                self.__visualizer.save_metrics(self.__CLASSIFIER_NAME, model_params[i], "tfidf",
+                                               report, micro, macro, weighted)
+            except:
+                logging.warning('Error on ' + model_params[i])
+            logging.warning(str(datetime.now()) + 'End ' + model_params[i])
+            i += 1
 
     def make_use_w2v(self):
         x_all = self.__vectors_provider.get_w2v_vectors()
@@ -199,12 +213,12 @@ class LabelPowersetExperiments:
         # logging.warning(str(datetime.now()) + 'End LogisticRegression')
 
         base_estimators = [
-            LogisticRegression(C=1.0, solver='sag', n_jobs=-1),
+            # LogisticRegression(C=1.0, solver='sag', n_jobs=-1),
             # LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
             #                    intercept_scaling=True, max_iter=100, multi_class='ovr', penalty='l2', random_state=None,
             #                    solver='newton-cg',
             #                    tol=0.0001, warm_start=False, n_jobs=-1),
-            # LinearSVC(),
+            LinearSVC(),
             # MLPClassifier()
             # SVC(C=0.1, cache_size=200, class_weight=None, coef0=0.0,
             #     decision_function_shape='ovr', degree=0, gamma='auto', kernel='linear',
@@ -213,12 +227,12 @@ class LabelPowersetExperiments:
         ]
 
         model_params = [
-            "LogisticRegression(C=1.0, solver='sag')",
+            # "LogisticRegression(C=1.0, solver='sag')",
             # "LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,\
             #                    intercept_scaling=True, max_iter=100, multi_class='ovr', penalty='l2', random_state=None,\
             #                    solver='newton-cg',\
             #                    tol=0.0001, warm_start=False, n_jobs=-1)",
-            # "LinearSVC()",
+            "LinearSVC()",
             # "MLPClassifier()",
             # "SVC(C=0.1, cache_size=200, class_weight=None, coef0=0.0,\
             #     decision_function_shape='ovr', degree=0, gamma='auto', kernel='linear',\
